5554acdef89a337a784acf27cc166a0bb0f15fbf
tsugita26 <tsugita@ucsd.edu>
2024-08-12 00:50:11 -0700

    fixed bug in manifest and tailwind + minor changes
    added empty popup, content script changed from React to vanilla

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Popup from "./popup/Popup"
 
 function App() { // start bilding your popup app here 
   return (
-    <h1 className="text-3xl text-center font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Popup />
+    </>
   )
 }
 
---
+++ src/popup/Popup.tsx
@@ -0,0 +1,7 @@
+export default function Popup() {
+  return (
+    <h1>
+      THIS IS A POPUP
+    </h1>
+  )
+}
