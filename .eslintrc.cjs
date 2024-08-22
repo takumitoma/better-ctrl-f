2fd7c24138333ed24e3ba5569ed6beb4048dfbc0
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-08-22 00:27:30 -0700

    chore: removed unused packages, updated eslint, added prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 'error',
   },
 }
