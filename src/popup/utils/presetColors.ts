7baeb1dc6e5da9739111125f8c9c4c72573f736e
takumitoma <85015271+takumitoma@users.noreply.github.com>
2024-09-11 17:44:08 -0700

    fix: make highlighting more legible

--- src/popup/utils/presetColors.ts
+++ src/popup/utils/presetColors.ts
@@ -1,20 +1,24 @@
 export const presetColors: string[] = [
   '#FFFF00', // Bright Yellow
-  '#00FFFF', // Cyan
-  '#90EE90', // Light Green
-  '#F08080', // Light Coral
-  '#87CEEB', // Sky Blue
-  '#DA70D6', // Orchid
-  '#FFDAB9', // Peach Puff
-  '#FFB6C1', // Light Pink
-  '#FAFAD2', // Light Goldenrod Yellow
   '#FFA500', // Orange
-  '#7DF9FF', // Electric Blue
+  '#D8BFD8', // Thistle
+  '#8E4585', // Plum
+  '#98FB98', // Pale Green
   '#32CD32', // Lime Green
-  '#FF7F50', // Coral
-  '#1E90FF', // Dodger Blue
-  '#BA55D3', // Medium Orchid
-  '#FFE4E1', // Misty Rose
-  '#FA8072', // Salmon
-  '#FFA07A', // Light Salmon
+  '#FFC0CB', // Pink
+  '#FF69B4', // Hot Pink
+  '#B0E0E6', // Powder Blue
+  '#4682B4', // Steel Blue
+  '#FFE4B5', // Moccasin
+  '#D2691E', // Chocolate
+  '#8B0000', // Dark Red
+  '#FF0000', // Red
+  '#006400', // Dark Green
+  '#00FF00', // Lime
+  '#00008B', // Dark Blue
+  '#0000FF', // Blue
+  '#4B0082', // Indigo
+  '#8A2BE2', // Blue Violet
+  '#2F4F4F', // Dark Slate Gray
+  '#00FFFF', // Cyan
 ];
